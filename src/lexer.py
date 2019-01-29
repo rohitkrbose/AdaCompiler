218fc1995dfcb362950790a6446abd3204a58231
RKB <rohit.kr.bose@gmail.com>
2019-01-29 12:28:28 +0530

    HTML Formatting done

--- src/lexer.py
+++ src/lexer.py
@@ -11,24 +11,25 @@ enc = createColDict(cfg_file) # Get keyword-to-colour mapping
 
 H = '<!DOCTYPE html><html><head><title>'+prog_name+'</title></head><body>' # HTML document output
 
-lexer = lex.lex()
 with open(prog_name) as fp:
     code = fp.read() + '\n'
-    lexer.input(code)
-    actstring = []
-    tokenstring = []
-    c_line = lexer.lineno
 
-    for token in lexer:
-        if(token.lineno != c_line) : # once we get to next line
-            H += getHTML(actstring,tokenstring,enc) # get colour-formatted HTML code for current line
-            actstring = []
-            tokenstring = []
-            c_line = token.lineno # update current line number
-        actstring.append(token.value)
-        tokenstring.append(str(token.type))
-    
-    H += getHTML(actstring,tokenstring,enc) # get colour-formatted HTML code for current line
+lexer = lex.lex() # Initialize lexer
+lexer.input(code)
+actstring = [] # Actual lexemes
+tokenstring = [] # Token types
+c_line = lexer.lineno
+
+for token in lexer:
+    if(token.lineno != c_line) : # once we get to next line
+        H += getHTML(actstring,tokenstring,enc) # get colour-formatted HTML code for current line
+        actstring = []
+        tokenstring = []
+        c_line = token.lineno # update current line number
+    actstring.append(token.value)
+    tokenstring.append(str(token.type))
+
+H += getHTML(actstring,tokenstring,enc) # get colour-formatted HTML code for current line
 
 H += '</body></html>'
 with open(output_file, 'w') as fp:
